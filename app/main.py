ab2553bf60c5586b473f7833435044bdfc1d8064
JAWAD-EMRE <jfjf0455@gmail.com>
2026-07-25 00:58:02 +0500

    fix: add production frontend URL to CORS whitelist

--- app/main.py
+++ app/main.py
@@ -13,17 +13,18 @@ from app.api.routes.auth import router as auth_router
 
 app = FastAPI(title="EduMind")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
+app.add_middleware( 
+    CORSMiddleware, 
+    allow_origins=[ 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173", 
+        "http://localhost:3000", 
         "http://127.0.0.1:3000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+        "https://edumind-frontend-production.up.railway.app"  # Add this line!
+    ], 
+    allow_credentials=True, 
+    allow_methods=["*"],  # Make sure this has an asterisk *
+    allow_headers=["*"],  # Make sure this has an asterisk *
 )
 
 app.include_router(users_router)
